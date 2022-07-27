7aa6baade8d8850d4ad3290bf41300663520d696
Snehal <Snehal@DESKTOP-UFUVF4F.hgu_lan>
2022-07-27 14:01:33 +0530

    Hii

---
+++ src/SamplePackage_2/Demo_2.java
@@ -0,0 +1,10 @@
+package SamplePackage_2;
+
+public class Demo_2 
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		System.out.println("a = "+a);
+	}
+}
---
+++ src/SamplePackage_2/Demo_3.java
@@ -0,0 +1,9 @@
+package SamplePackage_2;
+
+public class Demo_3 
+{
+public static void main(String[] args) 
+{
+	
+}
+}
